d500fac2c66d5dbd5d186629f342e8d8f6c052fa
Carolina Munoz <cmunoz@arrivelogistics.com>
2023-10-15 09:34:01 -0500

    README files small fix

--- app/src/test/java/com/sample/pages/LoginPage.java
+++ app/src/test/java/com/sample/pages/LoginPage.java
@@ -10,7 +10,7 @@ public class LoginPage extends Driver {
 
     public LoginPage() {
         super();
-        PageFactory.initElements(driver, this);
+        PageFactory.initElements(this.driver, this);
     }
 
 
